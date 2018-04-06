ed44dc6f1cf78a861251877218dea9e441330e0b
BRIANX2099 <37851160+BRIANX2099@users.noreply.github.com>
2018-04-06 20:43:59 +1200

    Add files via upload

---
+++ app/src/main/java/com/example/user/comp602_restaurant_mobile_application/paparichmenu.java
@@ -0,0 +1,16 @@
+package com.example.user.comp602_restaurant_mobile_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class paparichmenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paparichmenu);
+    }
+
+
+
+}
