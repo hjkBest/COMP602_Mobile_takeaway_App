1b2975d37bafb72b958ffd9b7270076c96b848a2
hjkBest <37132758+hjkBest@users.noreply.github.com>
2018-04-08 17:04:01 +1200

    made some change in Food.java
    Jiakun Hou

--- app/src/main/java/com/example/user/comp602_restaurant_mobile_application/Food.java
+++ app/src/main/java/com/example/user/comp602_restaurant_mobile_application/Food.java
@@ -3,12 +3,15 @@ package com.example.user.comp602_restaurant_mobile_application;
 public class Food {
     private int mFoodAmount;
     private String mFoodName;
-    private int mfoodPrice;
-    public  Food(int foodAmount,String foodName,int foodPrice)
+    private int mFoodPrice;
+    private String mProvidedBy;
+
+    public  Food(int foodAmount,String foodName,int foodPrice,String providedBy)
     {
         this.mFoodAmount=foodAmount;
         this.mFoodName=foodName;
-        this.mfoodPrice=foodPrice;
+        this.mFoodPrice=foodPrice;
+        this.mProvidedBy=providedBy;
     }
 
     public int getFoodAmount() {
@@ -19,8 +22,12 @@ public class Food {
         return mFoodName;
     }
 
-    public int getMfoodPrice() {
-        return mfoodPrice;
+    public int getFoodPrice() {
+        return mFoodPrice;
+    }
+
+    public String getProvidedBy() {
+        return mProvidedBy;
     }
 
     public void setFoodAmount(int foodAmount) {
@@ -31,7 +38,11 @@ public class Food {
         mFoodName = foodName;
     }
 
-    public void setMfoodPrice(int mfoodPrice) {
-        this.mfoodPrice = mfoodPrice;
+    public void setFoodPrice(int mfoodPrice) {
+        this.mFoodPrice = mfoodPrice;
+    }
+
+    public void setProvidedBy(String providedBy) {
+        mProvidedBy = providedBy;
     }
 }
